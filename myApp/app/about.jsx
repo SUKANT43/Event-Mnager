47d90c09a9ceebfddeead374dc73c14eb261ecba
sukant43 <sukant98657@gmail.com>
2026-03-15 19:10:30 +0530

    light and dark theme

--- myApp/app/about.jsx
+++ myApp/app/about.jsx
@@ -1,11 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, useColorScheme, View } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'
-
+import {Colors} from '../constants/Colors'
 const About = () => {
+  const colorScheme=useColorScheme();
+  const theme=Colors[colorScheme];
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>About page</Text>
+    <View style={[styles.container,{backgroundColor:theme.background}]}>
+      <Text style={[styles.title,{}]}>About page</Text>
       <Link href="/" style={styles.link}>Home Page</Link>
     </View>
   )
