047d2bb5d3ffc2e6fa36d72c24833729f8663f4f
sukant43 <sukant98657@gmail.com>
2026-03-15 19:51:54 +0530

    themed components added

--- myApp/app/index.jsx
+++ myApp/app/index.jsx
@@ -1,36 +1,51 @@
-import { StyleSheet, Text, View,Image } from 'react-native'
-import {Link} from 'expo-router'
+import { StyleSheet } from 'react-native'
+import { Link } from 'expo-router'
 
-import Logo from "../assets/images/logo_dark.png"
+import ThemedView from "../components/ThemedView"
+import ThemedText from "../components/ThemedText"
+import ThemedLogo from "../components/ThemedLogo"
+import Spacer from "../components/Spacer"
 
 const Home = () => {
   return (
-    <View style={styles.container}>
-        <Image source={Logo} style={styles.img}/>
-      <Text style={styles.title}>The Number 1</Text>
+    <ThemedView style={styles.container}>
+      <ThemedLogo />
+      <Spacer />
 
-      <Text style={{marginTop:10,marginTop:30}}>Reading List App</Text>
-    
-      <Link href='/about'>About Page</Link>
+      <ThemedText style={styles.title} title={true}>The Number 1</ThemedText>
 
-    </View>
+      <ThemedText style={{ marginTop: 10, marginBottom: 30 }}>
+        Reading List App
+      </ThemedText>
+
+      <Link href="/about" style={styles.link}>
+        <ThemedText>About Page</ThemedText>
+      </Link>
+
+      <Link href="/contact" style={styles.link}>
+        <ThemedText>Contact Page</ThemedText>
+      </Link>
+    </ThemedView>
   )
 }
 
 export default Home
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        alignItems:'center',
-        justifyContent:'center'
-    },
-    title:{
-        fontWeight:'bold',
-        fontSize:18
-    },
-    img:{
-        marginVertical:20,
-        backgroundColor:"black"
-    }
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  img: {
+    marginVertical: 20
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  link: {
+    marginVertical: 10,
+    borderBottomWidth: 1
+  }
 })
